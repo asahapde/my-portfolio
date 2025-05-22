f8081b666f4115617cf449c1dc59896be13f4c71
asahapde <60831459+asahapde@users.noreply.github.com>
2025-05-22 02:00:54 -0400

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="mx-auto w-full max-w-3xl md:max-w-4xl lg:max-w-5xl px-5">
+      <section
+        className="py-24 md:py-32"
+        style={{ backgroundColor: "var(--color-bg)" }}
+      >
+        <h1
+          className="mb-1.5 text-7xl font-bold tracking-tightest text-white sm:text-8xl md:mb-0 md:text-9xl"
+          style={{ fontFamily: "var(--font-heading)" }}
+        >
+          Abdullah Sahapdeen
+        </h1>
+        <p
+          className="mb-9 text-4xl leading-[46px] font-bold tracking-tighter text-sky-500 sm:text-5xl md:text-6xl"
+          style={{ fontFamily: "var(--font-heading)" }}
+        >
+          Software Engineer
+        </p>
+        <p className="mb-16 text-base font-normal text-gray-400 md:text-2xl">
+          I specialize in frontend and full-stack development, creating
+          user-friendly and visually appealing web applications. I have a
+          passion for coding and a keen eye for design, which allows me to build
+          seamless user experiences.
+        </p>
+        <a
+          className="inline-block text-base sm:text-lg md:text-xl rounded-full bg-sky-500 hover:bg-sky-700 px-8 py-5 leading-5 font-medium"
+          href="mailto:asahapde@gmail.com"
+        >
+          Get in Touch
+        </a>
+      </section>
+    </main>
+  );
+}
+
+export default App;
